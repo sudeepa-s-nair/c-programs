a6538bdbf4c502b4d88dbf128cb3461ce3d7e71e
Sudeepa <sudeepasnair11@gmail.com>
2023-05-07 08:03:42 -0400

    Sample c program for arithmetic operatiobs

---
+++ operations.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+
+int a,b,sum,diff,mul,div;
+
+printf("Enter two numbers: \n");
+scanf("%d %d",&a,&b);
+
+sum=a+b;
+diff=a-b;
+mul=a*b;
+div=a/b;
+
+printf("Sum is %d \n Difference is %d \n Product is %d \n Quotient is %d \n",sum,diff,mul,div);
+
+}
